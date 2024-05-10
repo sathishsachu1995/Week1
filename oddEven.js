605e8e8509dfbe76cda9f323d9b670a35dc86947
sathishsachu1995 <154673827+sathishsachu1995@users.noreply.github.com>
2024-05-10 15:58:29 +0530

    Add files via upload

---
+++ oddEven.js
@@ -0,0 +1,17 @@
+
+function isOddOrEven(inputNumber)
+{
+    if(inputNumber%2==0)
+    {
+        console.log(`The Given number `+inputNumber+` is even number`);
+
+    }
+    else
+    {
+        console.log(`The Given number `+inputNumber+` is odd number`);
+    }
+
+}
+
+isOddOrEven(17)
+
