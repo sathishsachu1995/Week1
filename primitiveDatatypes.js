822a90ae438a97cf10805a6494e559785afdddb7
sathishsachu1995 <154673827+sathishsachu1995@users.noreply.github.com>
2024-05-10 15:10:22 +0530

    Add files via upload

---
+++ primitiveDatatypes.js
@@ -0,0 +1,12 @@
+
+let firstName = `Sathish`
+let companyName = `Infosearch`
+let mobileNumber = 8667692118
+let isAutomation = true
+let hasPlaywright
+
+console.log(`The fistname is ${firstName} and its datatype is `+typeof(firstName));
+console.log(`The companyname is ${companyName} and its datatype is `+typeof(companyName));
+console.log(`The mobile number is ${mobileNumber} and its datatype is `+typeof(mobileNumber));
+console.log(`Is it a automation ${isAutomation} and its datatype is `+typeof(isAutomation));
+console.log(`Has playwright is ${hasPlaywright} and its datatype is `+typeof(hasPlaywright));
